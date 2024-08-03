ee4dfc59749e4e44417faabe2e3e11cf431c5ceb
Dayvid0063 <orjidavid63@gmail.com>
2024-08-03 12:03:42 +0100

    server new fix

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import dotenv from './utils/dotenv';
 import routes from './routes/index';
 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Load routes
-routes(app);
+app.use('/', routes);
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
