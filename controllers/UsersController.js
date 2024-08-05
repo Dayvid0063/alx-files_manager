1be771298dae92bd60eb2f38a4c44f915027f619
Dayvid0063 <orjidavid63@gmail.com>
2024-08-05 13:48:19 +0100

    recover 2

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,27 +1,23 @@
-// eslint-disable-next-line no-unused-vars
-import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+// eslint-disable-next-line import/no-named-as-default
+import redisClient from '../utils/redis';
 
 export default class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
-
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
-
     const existingUser = await dbClient.db.collection('users').findOne({ email });
     if (existingUser) {
       return res.status(400).json({ error: 'Already exist' });
     }
-
     const hashedPassword = sha1(password);
-
     const newUser = {
       email,
       password: hashedPassword,
@@ -29,9 +25,30 @@ export default class UsersController {
 
     try {
       const result = await dbClient.db.collection('users').insertOne(newUser);
-      return res.status(201).json({ id: result.insertedId, email: result.ops[0].email });
+      return res.status(201).json({ id: result.insertedId, email });
     } catch (error) {
       return res.status(500).json({ error: 'Error saving the user' });
     }
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
